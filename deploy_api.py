f8a72e6bc159f71f23e7308c1a18034f8b9272df
memlpl <136689048+memlpl@users.noreply.github.com>
2024-03-02 11:57:57 +0800

    Update deploy_api.py

--- deploy_api.py
+++ deploy_api.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request, HTTPException
 app = FastAPI()
 
-
 @app.get("/") 
-def read_root(): 
-    return {"message": "Hello World"}
+async def read_main(token: str = None):
+   if token != "your_secret_token":
+      raise HTTPException(status_code=401, detail="Unauthorized")
+   return {"message": "Hello, World!"}
